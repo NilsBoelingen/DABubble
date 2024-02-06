ba4c5e67a861a62ab8b26c939edbcd19fa109b2a
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-02-06 23:17:19 +0100

    add login

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-start-page',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, MatCardModule],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.scss'
 })
