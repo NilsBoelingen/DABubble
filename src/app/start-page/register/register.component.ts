c18b98cc300f36c5c85f97169d7d00e7192f1cb6
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-02-27 14:57:16 +0100

    work on form validation

--- src/app/start-page/register/register.component.ts
+++ src/app/start-page/register/register.component.ts
@@ -3,13 +3,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, Router } from '@angular/router';
 import { StartPageComponent } from '../start-page.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { FirebaseAuthService } from '../../services/firebase-auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, RouterModule, StartPageComponent, FormsModule],
+  imports: [MatButtonModule, MatIconModule, RouterModule, StartPageComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
@@ -18,9 +18,14 @@ export class RegisterComponent {
 
   isRunnig: boolean = true;
 
-  name:string = '';
+  name: string = '';
   email:string = '';
   password:string = '';
+
+  nameForm = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  emailForm = new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")]);
+  passwordForm = new FormControl('', [Validators.required]);
+  checkboxForm = new FormControl('', [Validators.required]);
 
   constructor(private router: Router) {}
 
